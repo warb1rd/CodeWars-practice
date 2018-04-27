ec6abd6bad5dae81f373b034b2ebeb62d9679510
Mahima Rao <mahima.m23@gmail.com>
2018-04-26 23:23:10 -0700

    codewars challenge

--- largest.js
+++ largest.js
@@ -1,15 +1,13 @@
 // find the oldest age in array
 
 
-function OldestAges(ages){
-    var largest = 0
-    var array = []
+function oldestAge(ages){
+    var oldest = 0
     for (i=0; i<ages.length; i++) {
-    if (ages[i]>largest) {
-        largest = ages[i]
-        array.push(largest);
+    if (ages[i]>oldest) {
+        oldest = ages[i]
         }
-        return array
     }
+    return oldest
 }
-OldestAges([2, 1, 6, 3])
+oldestAge([2, 1, 6, 3])
